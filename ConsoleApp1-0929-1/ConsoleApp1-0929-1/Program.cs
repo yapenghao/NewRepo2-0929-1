60c80cacbed24a3f45e1110b6beb3d87d4391859
yapeng hao <yapeng.hao@email.com>
2019-09-29 23:19:01 -0400

    test

--- ConsoleApp1-0929-1/ConsoleApp1-0929-1/Program.cs
+++ ConsoleApp1-0929-1/ConsoleApp1-0929-1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1_0929_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("master ");
+            Console.WriteLine("Dev");
         }
     }
 }
